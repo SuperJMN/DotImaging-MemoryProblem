a599c12ca814b7746340379fab046e25744ed3a9
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-08-10 12:46:34 +0200

    Add project files.

---
+++ ConsoleApplication8/SizeRetriever.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Threading.Tasks;
+    using DotImaging;
+    using DotImaging.Primitives2D;
+
+    public class SizeRetriever
+    {
+        public IObservable<Size> ImageSizeRetriever(IObservable<IImage> images)
+        {
+            return images
+                .SelectMany(image => Observable.FromAsync(() => GetSize(image)));
+        }
+
+        private static async Task<Size> GetSize(IImage image)
+        {
+            await Task.Delay(100);  // Simulate some delay
+            return image.Size;
+        }
+    }
+}
---
+++ ConsoleApplication8/ZipCapture.cs
@@ -0,0 +1,48 @@
+﻿namespace Sample
+{
+    using System.IO.Compression;
+    using System.Linq;
+    using DotImaging;
+
+    internal class ZipCapture : ImageStreamReader
+    {
+        private readonly ZipArchiveEntry[] allImages;
+        private readonly ZipArchive zipArchive;
+
+        public ZipCapture(ZipArchive zipArchive)
+        {
+            this.zipArchive = zipArchive;
+            var allImagesEnumerable = from entry in zipArchive.Entries
+                select entry;
+            allImages = allImagesEnumerable.ToArray();
+        }
+
+        public override bool CanSeek => true;
+
+        public override long Length => allImages.Length;
+
+        protected override bool ReadInternal(out IImage image)
+        {
+            var zipArchiveEntry = allImages[Position];
+            var length = zipArchiveEntry.Length;
+            using (var stream = zipArchiveEntry.Open())
+            {
+                var buffer = new byte[length];
+                stream.Read(buffer, 0, (int) length);
+                image = buffer.DecodeAsColorImage().Lock();
+            }
+
+            return true;
+        }
+
+        public override void Open()
+        {
+        }
+
+
+        public override void Close()
+        {
+            zipArchive.Dispose();
+        }
+    }
+}
