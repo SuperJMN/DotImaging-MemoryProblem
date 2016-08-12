70c7fbc2de51f49b5d3cffcc8d6df79fb4ec2814
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-08-12 10:04:27 +0200

    Restored

--- ConsoleApplication8/Program.cs
+++ ConsoleApplication8/Program.cs
@@ -25,15 +25,20 @@
 
         public static async Task Identify()
         {
+            var sources = new[] { "ZippedImages-Lite-1.zip", "ZippedImages-Lite-2.zip" };
             var identificator = new BatchIdentificator(new ImageIdentificator(new AutoRetryFaceClient(new FaceServiceClient("____REPLACE_ME_WITH_API_KEY____"))));
-            
-            var identifications = Observable.Using(() => CreateCapture("ZippedImages-Lite-1.zip"),
-                capture =>
-                {
-                    return identificator.Identify(capture.ToObservable(new EventLoopScheduler()), (i, idents) => new { i, idents });
-                });
 
-            await identifications.ToList();           
+            var identifications = sources
+                .ToObservable()
+                .SelectMany(
+                    path =>
+                        Observable.Using(() => CreateCapture(path),
+                            capture =>
+                            {
+                                return identificator.Identify(capture.ToObservable(), (i, idents) => new { i, idents });
+                            }));
+
+            await identifications.ToList();
         }
 
         private static ZipCapture CreateCapture(string path)
